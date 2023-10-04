1683f1e9e626e9a3c4218a8ca75ad07fffe23fcb
mymy <mymy.sawasa@karabiner.tech>
2023-10-05 00:25:00 +0900

    add_animation

---
+++ web_resson2/main.js
@@ -0,0 +1,29 @@
+$(".header__openbtn").click(function () {
+  $(this).toggleClass('active');
+});
+
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+  // ふわっ
+  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+    const elemPos = $(this).offset().top-50;//要素より、50px上の
+    const scroll = $(window).scrollTop();
+    const windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+    }else{
+    $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+    }
+    });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function (){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function(){
+  fadeAnime();/* アニメーション用の関数を呼ぶ*/
+});// 
