faf5ee9e64802b1b8997ed36abd1eeaa16b47d06
mymy <mymy.sawasa@karabiner.tech>
2023-10-19 13:40:17 +0900

    add_accordion

--- web_resson4/main.js
+++ web_resson4/main.js
@@ -5,16 +5,19 @@ const accordions = document.querySelectorAll(".content__accordion");
 accordions.forEach((accordion) => {
     // チェックボックスの要素を取得
     const checkbox = accordion.querySelector(".content__accordion__toggle");
-
-    // コンテンツの要素を取得
-    const content = accordion.querySelector("#hidden"); // "hidden"のIDを取得
+    const image = accordion.querySelector("#sign"); // アコーディオン内のアイコン
+    const content = accordion.querySelector("#hidden");
+    const plusImagePath = "./img/plus.svg";
+    const minusImagePath = "./img/minus.svg";
 
     // チェックボックスの状態を監視し、変更があった場合に"hidden"と"show"を切り替える
     checkbox.addEventListener("change", function () {
         if (checkbox.checked) {
             content.id = "show"; // IDを"show"に変更
+            image.src = minusImagePath;
         } else {
             content.id = "hidden"; // IDを"hidden"に変更
+            image.src = plusImagePath;
         }
     });
 });
